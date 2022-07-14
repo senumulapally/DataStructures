56457969e445dec84079b7f2a9f64aa11eaeb012
Snigdha Enumulapally <snigdha.enumulapally@gmail.com>
2022-07-14 11:42:50 -0700

    Updating right most index

--- BinarySearch/right_most_index.py
+++ BinarySearch/right_most_index.py
@@ -19,7 +19,7 @@ def getRightMostIndex(nums, target):
     """
     start = 0  # Initializing start-pointer
     end = len(nums) - 1  # Initializing end-pointer
-    flag = 0 # Initializing flag parameter with zero
+    index = -1  # Initializing flag parameter with zero
     while start <= end:
         ''' 
         While loop ends when start-pointer is greater than end-pointer. 
@@ -28,16 +28,16 @@ def getRightMostIndex(nums, target):
         '''
         mid = start + (end - start) // 2  # Calculating mid-point (Integer Overflow exception handled)
         if target == nums[mid]:
-            flag = mid  # Breaking the loop when the target value matched mid-pointer value
-            start = mid + 1  # Updating flag with the latest index of target value
+            index = mid  # Updating flag with the latest index of target value
+            start = mid + 1  # Moving the start-pointer to the next value as we are looking for right most index.
         elif target < nums[mid]:
             end = mid - 1  # Updating end-pointer when target is less than the mid-pointer value
         else:
             start = mid + 1  # Updating start-pointer when target is greater than the mid-pointer value
-    return flag
+    return index
 
 
 assert getRightMostIndex([1, 1, 1], 1) == 2  # Running Test Case 1
 assert getRightMostIndex([1], 1) == 0  # Running Test Case 2
 assert getRightMostIndex([1, 2, 2, 3, 3, 3, 4], 3) == 5  # Running Test Case 3
-assert getRightMostIndex([1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5], 4) == 10  # Running Test Case 4
+assert getRightMostIndex([1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5], 5) == 11  # Running Test Case 4
