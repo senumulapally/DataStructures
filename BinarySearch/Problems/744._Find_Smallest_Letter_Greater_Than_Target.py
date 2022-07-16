50ac8eb59da2d291da2fc8c3b22c2ba352da3bd1
Snigdha Enumulapally <snigdha.enumulapally@gmail.com>
2022-07-16 15:44:52 -0700

    LC744: Find Smallest Letter Greater Than Target

--- BinarySearch/Problems/744._Find_Smallest_Letter_Greater_Than_Target.py
+++ BinarySearch/Problems/744._Find_Smallest_Letter_Greater_Than_Target.py
@@ -11,10 +11,13 @@ class Solution(object):
         if target < letters[end]:  # When the target letter is greater than the greatest letter in the list,
             # Index out of range error is occurred
 
-            while start <= end:  # Once the start>end the loop ends, we shall return the start-pointer
+            while start < end:  # Once the start>end the loop ends, we shall return the start-pointer
                 # as it points to the next least element of the target
                 mid = start + (end - start) // 2  # Calculating the mid-point.
-                if letters[mid] < target:  # When the mid-point letter is less than the given target,
+                if letters[
+                    mid] == target:  # If the mid-point value is equal to the target, next point value is returned
+                    return letters[mid + 1]
+                elif letters[mid] < target:  # When the mid-point letter is less than the given target,
                     # start-pointer is updated to the next point of mid-point
                     start = mid + 1
                 else:  # Else, end-pointer is updated to the previous point of mid-point
@@ -28,7 +31,7 @@ class Solution(object):
 
 obj1 = Solution()
 assert obj1.nextGreatestLetter(["c", "f", "j"], "a") == "c"  # Test Case 1
-assert obj1.nextGreatestLetter(["c", "f", "j"], "f") == "f"  # Test Case 2
+assert obj1.nextGreatestLetter(["c", "f", "j"], "f") == "j"  # Test Case 2
 assert obj1.nextGreatestLetter(["a", "b", "c", "f", "f"], "d") == "f"  # Test Case 3
 assert obj1.nextGreatestLetter(["a", "b", "c", "f"], "g") == "Range exceeded"  # Test Case 4
 assert obj1.nextGreatestLetter(["a", "b", "c", "f", "h"], "g") == "h"  # Test Case 5
