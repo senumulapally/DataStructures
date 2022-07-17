489fd0b16658d032ffa4d097e02cdaf8831b7cad
Snigdha Enumulapally <snigdha.enumulapally@gmail.com>
2022-07-16 17:33:46 -0700

    LC744: Find Smallest Letter Greater Than Target

--- BinarySearch/Problems/744._Find_Smallest_Letter_Greater_Than_Target.py
+++ BinarySearch/Problems/744._Find_Smallest_Letter_Greater_Than_Target.py
@@ -8,32 +8,27 @@ class Solution(object):
         start = 0  # Initiating start-pointer
         end = len(letters) - 1  # Initiating end-pointer
 
-        if target < letters[end]:  # When the target letter is greater than the greatest letter in the list,
+        if target >= letters[end]:  # When the target letter is greater than the greatest letter in the list,
             # Index out of range error is occurred
+            return letters[0]  # This text is returned when the selected target's index is out of range.
 
-            while start <= end:  # Once the start>end the loop ends, we shall return the start-pointer
-                # as it points to the next least element of the target
-                mid = start + (end - start) // 2  # Calculating the mid-point.
-                if letters[
-                    mid] == target:  # If the mid-point value is equal to the target, next point value is returned
-                    return letters[mid + 1]
-                elif letters[mid] < target:  # When the mid-point letter is less than the given target,
-                    # start-pointer is updated to the next point of mid-point
-                    start = mid + 1
-                else:  # Else, end-pointer is updated to the previous point of mid-point
-                    end = mid - 1
+        while start <= end:  # Once the start>end the loop ends, we shall return the start-pointer
+            # as it points to the next least element of the target
+            mid = start + (end - start) // 2  # Calculating the mid-point.
+            if letters[mid] <= target:  # When the mid-point letter is less than or equal to the given target,
+                # start-pointer is updated to the next point of mid-point
+                start = mid + 1
+            else:  # Else, end-pointer is updated to the previous point of mid-point
+                end = mid - 1
 
-            return letters[start]  # By the end of this loop, start pointer will be pointed to the next value
-            # of the target. Hence, it is returned
-
-        return "Range exceeded"  # This text is returned when the selected target's index is out of range.
-
+        return letters[start]  # By the end of this loop, start pointer will be pointed to the next value
+        # of the target. Hence, it is returned
 
 obj1 = Solution()
 assert obj1.nextGreatestLetter(["c", "f", "j"], "a") == "c"  # Test Case 1
 assert obj1.nextGreatestLetter(["c", "f", "j"], "f") == "j"  # Test Case 2
 assert obj1.nextGreatestLetter(["a", "b", "c", "f", "f"], "d") == "f"  # Test Case 3
-assert obj1.nextGreatestLetter(["a", "b", "c", "f"], "g") == "Range exceeded"  # Test Case 4
+assert obj1.nextGreatestLetter(["a", "b", "c", "f"], "g") == "a"  # Test Case 4
 assert obj1.nextGreatestLetter(["a", "b", "c", "f", "h"], "g") == "h"  # Test Case 5
 assert obj1.nextGreatestLetter(["a", "b", "c", "c", "f", "h"], "g") == "h"  # Test Case 6
 assert obj1.nextGreatestLetter(["e", "e", "e", "e", "e", "e", "n", "n", "n", "n"], "e") == "n"  # Test Case 7
