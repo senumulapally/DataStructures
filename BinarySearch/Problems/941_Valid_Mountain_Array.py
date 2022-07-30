6c70435a4ffb162e602468260b0d4819e16c90a9
Snigdha Enumulapally <snigdha.enumulapally@gmail.com>
2022-07-29 19:28:19 -0700

    LC941: Valid Mountain Array

--- BinarySearch/Problems/941_Valid_Mountain_Array.py
+++ BinarySearch/Problems/941_Valid_Mountain_Array.py
@@ -14,20 +14,21 @@ def validMountainArray(arr):
     pos = -1
     neg = -1
     while i < high:
-        if arr[i + 1] - arr[i] > 0:
+        if arr[i + 1] - arr[i] > 0:  # If increment in two consecutive numbers pos = 0
             pos = 0
-        elif arr[i + 1] - arr[i] < 0:
+        elif arr[i + 1] - arr[i] < 0: # If decrement in two consecutive numbers neg = 0
             neg = 0
-        else:
+        else:  # When both are equal returning False
             return False
-        if arr[i + 1] - arr[i] > 0 and neg == 0:
+        if arr[i + 1] - arr[i] > 0 and neg == 0:  # Once the negative is already 0 and then the values start
+            # increasing, returning false
             return False
         i += 1
 
-    if pos == -1 or neg == -1:
+    if pos == -1 or neg == -1:  # if either one of them is missing, returning false
         return False
 
-    return True
+    return True  # returning true when all the above conditions are satisfied.
 
 
 print(validMountainArray([2, 1]))
@@ -35,3 +36,4 @@ print(validMountainArray([3, 5, 5]))
 print(validMountainArray([0, 3, 2, 1]))
 print(validMountainArray([1]))
 print(validMountainArray([0, 1, 0]))
+print(validMountainArray([-1, -2, 0]))
