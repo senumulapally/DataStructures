bd86af2dd54de5a82d9f7312f16b030a52e775ce
Snigdha Enumulapally <snigdha.enumulapally@gmail.com>
2022-10-17 23:56:45 -0700

    Invert Binary Tree

---
+++ Trees/Binary_Tree/226_Invert_Binary_Tree.py
@@ -0,0 +1,27 @@
+"""Given the root of a binary tree, invert the tree, and return its root."""
+"""Sol: Write a recursive function to swap the lrft and right nodes for root value and 
+recursively call the function for left and right nodes until 'None' is reached."""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        self.recursive(root)
+        return root
+
+    def recursive(self, root):
+        if root is None:
+            return None
+
+        root.left, root.right = root.right, root.left
+        self.recursive(root.left)
+        self.recursive(root.right)
+
